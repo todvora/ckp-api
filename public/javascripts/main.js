228caf4c1b3a2bd7058b5ef89366c7e4833b1b46
Tomas Dvorak <todvora@gmail.com>
2014-04-06 16:59:38 +0200

    aggregator - data for last 48 months

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
     $('#submit').click(function() {
         var regno = $("#regno").val();
         var currentDate = new Date();
-        var formatedDate = currentDate.getDate() + "." + currentDate.getMonth() +"."+ currentDate.getFullYear();
+        var formatedDate = currentDate.getDate() + "." + (currentDate.getMonth()+1) +"."+ currentDate.getFullYear();
         $.getJSON('/api', { 'regno':regno, 'date':formatedDate}, function(data) {
-            console.log(data);
+
                 var container = document.getElementById('chart');
                 var chart = new google.visualization.Timeline(container);
                 var dataTable = new google.visualization.DataTable();
@@ -14,15 +14,20 @@ $(document).ready(function(){
             dataTable.addColumn({ type: 'date', id: 'Konec' });
 
 
-            var startDate = new Date(data.date_from);
-            var endDate = new Date();
-            if(data.date_till != null) {
-               endDate = new Date(data.date_till);
-            }
+            var rows = [];
+            $.each(data, function(index,item){
+                console.log(item);
+                var startDate = new Date(item.date_from);
+                var endDate = new Date();
+                if(item.date_till != null) {
+                    endDate = new Date(item.date_till);
+                }
+                var entry = [item.company.name, startDate, endDate];
+                rows.push(entry);
+            });
 
-            data
-            dataTable.addRows([
-                [ data.company.name,  startDate,  endDate ]]);
+
+            dataTable.addRows(rows);
 
             chart.draw(dataTable);
         });
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,14 @@
 var ckp = require("../lib/parser/ckp");
 var client = new ckp.Client();
+var aggregator = require("../lib/parser/aggregator");
+var aggregatorClient = new aggregator.Client();
+
 
 module.exports = function (app) {
     app.get('/api', function (req, res) {
         var regno = req.query["regno"];
         var date = req.query["date"];
-        client.search(regno, date, function (results) {
+        aggregatorClient.search(regno, date, function (results) {
             res.set('Content-Type', "text/javascript; charset=utf-8");
             res.render('api',
                 {
