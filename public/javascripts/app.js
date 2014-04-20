0ed3f1ddead49ea4a231f0426913746c46ba74fb
Tomas Dvorak <todvora@gmail.com>
2014-04-20 17:17:42 +0200

    insured and not insured intervals, printed dates

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,81 +1,107 @@
-var insurance = Backbone.Model.extend({
+var InsuranceItemModel = Backbone.Model.extend({
     end: null,
     start: null,
     value: null
 });
 
-var insuranceCollection = Backbone.Collection.extend({
-    model: insurance,
+var InsuranceCollectionModel = Backbone.Collection.extend({
+    setRegistrationNumber: function (regno) {
+        this.regno = regno;
+        return this;
+    },
+
     url: function () {
         var currentDate = new Date();
         var formatedDate = currentDate.getDate() + "." + (currentDate.getMonth() + 1) + "." + currentDate.getFullYear();
         return "/api?&regno=" + this.regno + "&date=" + formatedDate;
     }
 });
-_.extend(insuranceCollection, Backbone.Events);
 
-collection = new insuranceCollection();
+_.extend(InsuranceCollectionModel, Backbone.Events);
+
+function dateToString(date) {
+    if(date != null) {
+        return date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
+    }
+    return "neuvedeno";
+}
+
+function checkNotInsured(items) {
+    var intervals = "";
+    _.each(items, function (item) {
+        if(item.get("value") === null) {
+            var from = new Date(item.get("start"));
+            var till = new Date(item.get("end"));
+            intervals = intervals + "<li>" + dateToString(from) + " - " + dateToString(till) + "</li>";
+        }
+    });
+    $("#notinsured").show();
+    $("#notInsuredDates").html(intervals);
+}
+
+function checkInsured(items) {
+    var intervals = "";
+    _.each(items, function (item) {
+        var value = item.get("value");
+        if(value != null) {
+            intervals = intervals + "<li>" + value.period + " "+ value.company.name + "</li>";
+        }
+    });
+    $("#insured").show();
+    $("#insuredDates").html(intervals);
+}
+
+function renderItems(chart, items) {
+    var dataTable = new google.visualization.DataTable();
+    dataTable.addColumn({ type: 'string', id: 'Pojištění' });
+    dataTable.addColumn({ type: 'date', id: 'Začátek' });
+    dataTable.addColumn({ type: 'date', id: 'Konec' });
+
+    var rows = [];
+    _.each(items, function (item) {
+        if (item.get("value") != null) {
+            item = item.get("value");
+            var startDate = new Date(item.date_from);
+            var endDate = new Date();
+            if (item.date_till != null) {
+                endDate = new Date(item.date_till);
+            }
+            $("#type").html(item.manufacturer + " - " + item.spz + ", " + item.type);
+            rows.push([item.company.name, startDate, endDate]);
+        } else {
+            rows.push(["NEPOJIŠTĚNO!", new Date(item.get("start")), new Date(item.get("end"))]);
+        }
+    });
+
+    dataTable.addRows(rows);
+    chart.draw(dataTable);
+}
 
 CollectionView = Backbone.View.extend({
-
-    el: $('#chart'),
-
     initialize: function () {
-        console.log("init");
         this.model.on('reset', this.render);
     },
-
     render: function (event) {
-        console.log(this);
         var self = this;
-
         var container = document.getElementById('chart');
         var chart = new google.visualization.Timeline(container);
-        var dataTable = new google.visualization.DataTable();
-
-        dataTable.addColumn({ type: 'string', id: 'Pojištění' });
-        dataTable.addColumn({ type: 'date', id: 'Začátek' });
-        dataTable.addColumn({ type: 'date', id: 'Konec' });
-
-        var rows = [];
-
-        _.each(this.models, function (item) {
-            if (item.get("value") != null) {
-                item = item.get("value");
-                var startDate = new Date(item.date_from);
-                var endDate = new Date();
-                if (item.date_till != null) {
-                    endDate = new Date(item.date_till);
-                }
-
-                $("#type").html(item.manufacturer + " - " + item.spz + ", " + item.type);
-
-                var entry = [item.company.name, startDate, endDate];
-                rows.push(entry);
-            } else {
-                var entry = ["NEPOJIŠTĚNO!", new Date(item.get("start")), new Date(item.get("end"))];
-                rows.push(entry);
-                $("#notinsured").show();
-            }
-        });
-
-        dataTable.addRows(rows);
-
-        chart.draw(dataTable);
-
+        renderItems(chart, this.models);
+        checkNotInsured(this.models);
+        checkInsured(this.models);
         return this;
     }
-
 });
 
+var collection = new InsuranceCollectionModel();
 new CollectionView({model: collection});
 
-//wherever you need to do the ajax
 $('#submit').click(function () {
+
     var button = $(this);
     button.button('loading');
+
     var regno = $("#regno").val();
-    collection.regno = regno;
+    collection.setRegistrationNumber(regno);
     collection.fetch({
         success: function (model, response, options) {
             button.button('reset');
